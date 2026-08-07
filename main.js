379a43b1549e8e944b048f70a0adbbd5b07c0033
hp-5222 <happlestudio5222@gmail.com>
2026-08-07 00:51:53 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,72 @@
-console.log("Hello, world!");
+async function getWeeklyCharts() {
+  const apiKey = 'd84c9b2caa4ff06ef2c35d5ba07f7f02';
+  const username = 'happle5222';
+
+  const url = `https://ws.audioscrobbler.com/2.0/?method=user.getweeklychartlist&user=${username}&api_key=${apiKey}&format=json`;
+
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    return data;
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+async function getWeeklyAlbumChart(from,to) {
+  const apiKey = 'd84c9b2caa4ff06ef2c35d5ba07f7f02';
+  const username = 'happle5222';
+
+  const url = `https://ws.audioscrobbler.com/2.0/?method=user.getWeeklyAlbumChart&user=${username}&api_key=${apiKey}&from=${from}&to=${to}&format=json`;
+
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    return data;
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+async function main() {
+  let weeklyCharts = await getWeeklyCharts();
+  let mostRecentPeriod = weeklyCharts.weeklychartlist.chart[weeklyCharts.weeklychartlist.chart.length-1];
+  let from = mostRecentPeriod.from;
+  let to = mostRecentPeriod.to;
+  let topAlbums = await getWeeklyAlbumChart(from,to);
+  let priorWeek = weeklyCharts.weeklychartlist.chart[weeklyCharts.weeklychartlist.chart.length - 2];
+  let priorFrom = priorWeek.from;
+  let priorTo = priorWeek.to;
+  let priorTopAlbums = await getWeeklyAlbumChart(priorFrom,priorTo);
+
+  for (let i=1; i<21; i++) {
+
+    // console.log(new Date(parseInt(from,10)*1000).toLocaleDateString('en-US'));
+    // console.log(new Date(parseInt(to,10)*1000).toLocaleDateString('en-US'));
+
+    let selectedAlbum = topAlbums.weeklyalbumchart.album[i-1].name;
+    let selectedArtist = topAlbums.weeklyalbumchart.album[i-1].artist["#text"];
+    let selectedPlays = topAlbums.weeklyalbumchart.album[i-1].playcount;
+    console.log(selectedAlbum + " - " + selectedArtist + " - " + selectedPlays + " plays");
+
+    let foundAlbum = priorTopAlbums.weeklyalbumchart.album.find(album =>
+      album.name === selectedAlbum &&
+      album.artist["#text"] === selectedArtist
+    );
+    if (foundAlbum) {
+      let previousWeekRank = parseInt(foundAlbum["@attr"].rank,10);
+      let upOrDown = i < previousWeekRank ? "▲ " : "▼ ";
+      let finalStr = upOrDown + String(Math.abs(i-previousWeekRank));
+      if (i-previousWeekRank == 0) {
+        console.log("▶")
+      } else {
+        console.log(finalStr);
+      }
+    } else {
+      console.log("NEW")
+    }
+    console.log("-------------------")
+  }
+}
+
+main()
